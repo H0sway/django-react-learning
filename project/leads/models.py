bc853f720c19b4a05c03c9c368977c262bf23bd5
Joshua Russell <jkrussell756@gmail.com>
2018-08-07 10:58:02 -0400

    image upload fully working as intended, had to change some things around in the models. added a default image

--- project/leads/models.py
+++ project/leads/models.py
@@ -5,5 +5,5 @@ class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.CharField(max_length=300)
-    picture = models.ImageField(null=True, upload_to="../frontend/static/media")
+    picture = models.ImageField(default = "default.png")
     created_at = models.DateTimeField(auto_now_add=True)
